f68bfde0f50f16eab9f14ff28ef188ea45e7e2f8
mrglebgleb17 <svskanka17@duck.com>
2024-09-21 11:41:31 -0500

    Create chapter2.py
    need to add section on input and number evaluations(<,>,==,etc)

---
+++ chapter2.py
@@ -0,0 +1,18 @@
+"""py0ch2 2,2,2
+operations 2.1 and inputs
+Operators are things that make binary decisions in python with two inputs
+operations are performed with the use of keywords like "and","or" and "not"
+
+AND operation 2.2
+the and operation only outputs True if both the inputs are true"""
+a=True
+b=False
+print(a and a)
+print(a and b)
+#OR operation 2.3
+#if one of the inputs is true,the output is true
+print(a or b)
+print(b or b)
+#NOT operation 2.4
+#this reverses the input so if the input is true,the output is false and vice-versa
+print(not a)
