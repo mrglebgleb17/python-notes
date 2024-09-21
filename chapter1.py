320307da296ef874d395d15f0e6fbb92fcc5f356
mrglebgleb17 <svskanka17@duck.com>
2024-09-21 11:39:32 -0500

    Create chapter1.py
    notes for variables, data types, comments, and print

---
+++ chapter1.py
@@ -0,0 +1,34 @@
+#py0ch1 1,2,4
+#this is a comment.comments are made using the hashtag#
+#comments are not part of the code and are used to annotate or explain what the code is doing
+'''three apostrophies
+can
+make
+multilined
+comments'''
+#the print command can print whatever you put in it as long as its in "quotes". this data is known as strings
+print("quotes")
+#variables can store data in them, and that data can be changed and added to another variable(like in math!)
+a=1
+b=2
+c=a+b
+#you can print variables as long as they are not in quotes
+print(c)#prints the value of c (which is 3)
+print("c")#prints the letter c
+#you can print variables and strings in the same print statement & seperated by a comma
+print("1+2 is",c)
+#in variabes,you can store many different data types
+#string
+a="strings"
+#integer
+b=5
+#float(decimals)
+c=2.71
+#boolean  True or False(the first letter MUST be capitalized)
+d=True
+print(a,b,c,d)
+#just like how you can combine 1 and 1 to get 2,you can combine multiple strings!
+#ex:
+b=" and more strings "
+c=a+b
+print(c)
